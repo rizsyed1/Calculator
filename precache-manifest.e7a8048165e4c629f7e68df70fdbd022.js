377a2fc3e06af9ca07f71364f84cc4816cad22df
RizwanSyed357 <rizwansyed357@protonmail.com>
2019-03-27 21:00:13 +0000

    Updates

---
+++ precache-manifest.e7a8048165e4c629f7e68df70fdbd022.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf49d768c4d3a5b9fc25",
+    "url": "/Calculator/static/js/runtime~main.8078b280.js"
+  },
+  {
+    "revision": "09298e50bcc7e443b5c5",
+    "url": "/Calculator/static/js/main.5a695b09.chunk.js"
+  },
+  {
+    "revision": "62572074add44b1a95b1",
+    "url": "/Calculator/static/js/2.e978d812.chunk.js"
+  },
+  {
+    "revision": "09298e50bcc7e443b5c5",
+    "url": "/Calculator/static/css/main.fa5e0c29.chunk.css"
+  },
+  {
+    "revision": "f4b90e4f3e20ca63d76dda505da81eac",
+    "url": "/Calculator/index.html"
+  }
+];
